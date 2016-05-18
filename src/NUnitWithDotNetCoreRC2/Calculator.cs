43642e74229dd6c5bff46485be526889d05be45f
Rob Prouse <rob@prouse.org>
2016-05-18 11:06:45 -0400

    Initial checkin of the RC2 test project

---
+++ src/NUnitWithDotNetCoreRC2/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace NUnitWithDotNetCoreRC2
+{
+    public static class Calculator
+    {
+        public static int Add(int x, int y) => x + y;
+        public static int Subtract(int x, int y) => x - y;
+        public static int Multiply(int x, int y) => x * y;
+        public static int Divide(int x, int y) => x / y;
+    }
+}
